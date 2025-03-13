a15b15a9ef5ef038559548eaa68a1706ff1c0ee2
dzodze00 <pakpablie@gmail.com>
2025-03-13 16:26:03 -0700

    Create page.tsx

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Test Page</h1>
+      <p>If you can see this page, your deployment is working correctly!</p>
+    </div>
+  )
+}
